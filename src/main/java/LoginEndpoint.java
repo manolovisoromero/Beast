495abb25ad06162afa6722a87e49b867b2afbff8
Manolo Viso Romero <manolovisoromero@hotmail.com>
2019-09-21 21:47:25 +0200

    htttps werkend

--- src/main/java/LoginEndpoint.java
+++ src/main/java/LoginEndpoint.java
@@ -9,10 +9,19 @@ public class LoginEndpoint {
 
 
     // region get
-    @Path("/{login}")
+    @Path("/hello/{login}")
     @Produces(MediaType.TEXT_PLAIN)
     @GET
-    public Response getLogin(@PathParam("login") String name) {
-        return Response.status(200).entity("Hello").build();
+    public Response getLogin(
+            @PathParam("login") String name) {
+        return Response.status(200).entity("Hello" + name).build();
+    }
+
+    @Path("/bye/{login}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @GET
+    public Response getGoodbye(
+            @PathParam("login") String name) {
+        return Response.status(200).entity("Goodbye"+name).build();
     }
 }
